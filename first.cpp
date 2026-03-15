53aee50a8d4dc81964f5828d5a2a25baa31533e8
Nishant Sharma <iamnishant32@gmail.com>
2026-03-15 13:46:17 +0530

    second commit

---
+++ first.cpp
@@ -0,0 +1,25 @@
+// Author: Nishant Sharma
+// Created: 2026-03-15 13:28:07
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = {0, 1, 0, -1};
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cout << "hi this is nishant";
+
+    int n; cin >> n;
+    cout << n;
+
+}
